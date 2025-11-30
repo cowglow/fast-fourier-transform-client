51ffadcecccb7d690a7184e9326143dbdeb32c88
Philip Saa <cowglow@gmail.com>
2025-11-30 08:56:31 +0100

    Add base param

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/fast-fourier-transform-client",
     plugins: [react()],
     server: {
         proxy: {
